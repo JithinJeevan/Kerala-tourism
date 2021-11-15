bd1e92b209181eeb9803fc1cd6f96210c7236c8f
JithinJeevan <93697234+JithinJeevan@users.noreply.github.com>
2021-11-15 12:57:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function firstValidate(){
     }
 }
 function secondValidate(){
-    var regExp2 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{5,}$/
+    var regExp2 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{8,}$/
 
     if (regExp2.test(loginPwd.value)) {
        
