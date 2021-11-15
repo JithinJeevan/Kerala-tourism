222b61705d24dedc47efd59b43eff47627a6472b
JithinJeevan <93697234+JithinJeevan@users.noreply.github.com>
2021-11-15 16:02:49 +0530

    very new change

--- signUp.js
+++ signUp.js
@@ -4,9 +4,9 @@ var errorName = document.getElementById("errorName");
 var num = document.getElementById("inputNumber");
 var errorNum = document.getElementById("errorNumber");
 var regEmail = document.getElementById("inputEmail4");
-var errorEmail = document.getElementById("errorRegemail");
+var errorE = document.getElementById("errorRegemail");
 var regPwd = document.getElementById("inputPassword4");
-var errorPwd = document.getElementById("errorRegpwd");
+var errorP = document.getElementById("errorRegpwd");
 var rePwd = document.getElementById("rePwd");
 var errorRepwd = document.getElementById("errorRepwd");
 var strong = document.getElementById("weak");
@@ -18,24 +18,33 @@ var bar3 = document.getElementById("bar3");
 function nameValidate(){
     var reg1= /^[A-Za-z]+$/
 
-    if (reg1.test (nam.value && name2.value)){
+    if ( reg1.test(name2.value)&& reg1.test (nam.value )){
         return true;
 
     }
-    else{
-        errorName.innerHTML="Give proper name";
+
+    else if (name2.length==0 || nam.length==0){
+        errorName.innerHTML="Type Name";
+        return false; 
     }
+    
+        else{
+            errorName.innerHTML="Give Proper Name";
+            errorName.style.color="red";
+            return false;
+        }
 }
 // Number validation
 function numberValidate(){
    var reg2= /^([1-9])([\d]{2}[-\s.]?)([\d]{3}[-\s.]?)([\d]{4})$/
 
     if (reg2.test(num.value)) {
-        errorNum.innerHTML="Mobile Number is Valid";
+        
         return true;
     }
     else{
         errorNum.innerHTML="Mobile Number is not Valid";
+        errorNum.style.color="red";
         return false;
     }
 }
@@ -45,15 +54,12 @@ function validate(){
     
     if(regExp1.test(regEmail.value) ){
 
-        errorEmail.innerHTML= "Valid";
         
-        
-        errorEmail.style.color="green";
         return true;
     }
     else{
-        errorEmail.innerHTML= "Invalid Email ";
-        errorEmail.style.color="red";
+        errorE.innerHTML= "Invalid Email ";
+        errorE.style.color="red";
         
         return false;
     }
@@ -70,7 +76,7 @@ function validate1(){
     }
     else{
         errorPwd.innerHTML="Invalid Password";
-        errorPwd.style.color="red";
+        errorP.style.color="red";
         return false;
     }
 }
@@ -88,51 +94,51 @@ function checkPassword(){
         }
     }
 
-    
-    
- regPwd.addEventListener("keyup",function (){
-
-    var lng=regPwd.value;
      
-    var regExp3 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{6,8}$/
-    var regExp4 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{9,12}$/
-    var regExp5 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{13,}$/
     
-    
-    
-     if(lng=="" && lng.length==0) {
-         bar1.style.backgroundColor="gray";
-         bar2.style.backgroundColor="gray";
-         bar3.style.backgroundColor="gray";
-     } 
-    else if( lng.length<=5   ) {
-        bar1.style.backgroundColor="red";
-        bar2.style.backgroundColor="gray";
-        bar3.style.backgroundColor="gray";
-        strong.innerHTML="Password Strength:  Not Enough";
-    }  
-    
-    else if( 6<=lng.length<=8 && regPwd.value.match(regExp3)  ) {
-        bar1.style.backgroundColor="green";
-        bar2.style.backgroundColor="gray";
-        bar3.style.backgroundColor="gray";
-        strong.innerHTML="Password Strength:  Weak";
-    } 
-    else if( 9<=lng.length<=12 && regPwd.value.match(regExp4)  ) {
-        bar1.style.backgroundColor="green";
-        bar2.style.backgroundColor="green";
-        bar3.style.backgroundColor="gray";
-        strong.innerHTML="Password Strength:  Medium";
-    }  
-    else if( 13<=lng.length && regPwd.value.match(regExp5)  ) {
-        bar1.style.backgroundColor="green";
-        bar2.style.backgroundColor="green";
-        bar3.style.backgroundColor="green";
-        strong.innerHTML="Password Strength:  Strong";
-    }  
+    document.getElementById("inputPassword4").addEventListener("keyup",function (){
 
-    }
- )
+        var lng=regPwd.value;
+         
+        var regExp3 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{6,8}$/
+        var regExp4 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{9,12}$/
+        var regExp5 = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{13,}$/
+        
+        
+        
+         if(lng=="" && lng.length==0) {
+             bar1.style.backgroundColor="gray";
+             bar2.style.backgroundColor="gray";
+             bar3.style.backgroundColor="gray";
+         } 
+        else if( lng.length<=5   ) {
+            bar1.style.backgroundColor="red";
+            bar2.style.backgroundColor="gray";
+            bar3.style.backgroundColor="gray";
+            strong.innerHTML="Password Strength:  Not Enough";
+        }  
+        
+        else if( 6<=lng.length<=8 && regPwd.value.match(regExp3)  ) {
+            bar1.style.backgroundColor="green";
+            bar2.style.backgroundColor="gray";
+            bar3.style.backgroundColor="gray";
+            strong.innerHTML="Password Strength:  Weak";
+        } 
+        else if( 9<=lng.length<=12 && regPwd.value.match(regExp4)  ) {
+            bar1.style.backgroundColor="green";
+            bar2.style.backgroundColor="green";
+            bar3.style.backgroundColor="gray";
+            strong.innerHTML="Password Strength:  Medium";
+        }  
+        else if( 13<=lng.length && regPwd.value.match(regExp5)  ) {
+            bar1.style.backgroundColor="green";
+            bar2.style.backgroundColor="green";
+            bar3.style.backgroundColor="green";
+            strong.innerHTML="Password Strength:  Strong";
+        }  
+    
+        }
+     )
     
         
     
